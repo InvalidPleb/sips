7d4b49b6464ece2aa5c9b3e1e48b20c32206e1a8
InvalidPleb <invalidpleb1@gmail.com>
2015-12-04 12:20:16 -0700

    Added input fields + btn to make more

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,55 @@
 'use strict';
 
-/**
+/*
  * @ngdoc function
  * @name statscalcApp.controller:MainCtrl
  * @description
  * # MainCtrl
  * Controller of the statscalcApp
  */
+
 angular.module('statscalcApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', function ($scope) {
+
+  	var i;
+    $scope.inputs = [];
+
+    $scope.addfield = function() {
+
+    	$scope.cntInputs += 1;
+
+    //	for (i=1; i <= 10; i++) {
+    //		$scope.inputs.push(i);
+    //	}
+ 
+    };
+
+    $scope.debugBtn = function() {
+    	
+    	console.log($scope.inputs);
+    };
+
+    $scope.cntInputs = 1;
+    
+    
+    $scope.getTimes=function(){
+	    var a = [];
+
+	    for(i=1; i <= $scope.cntInputs; i++) {
+			a.push(i);
+	    }
+
+	    return a;	    
+	};
+    
+    $scope.getValue = function(){
+        //here get the value of that inserted in the element with the id of "input_" + id
+        return $scope.inputs[-1];
+    };
+
+
+
+
+
+
+});
