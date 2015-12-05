e4d2601bcd6b9ce0887e80322621cf8720e1cc2a
InvalidPleb <invalidpleb1@gmail.com>
2015-12-05 15:36:36 -0700

    Working on excel-like table

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,36 +12,27 @@ angular.module('statscalcApp')
   .controller('MainCtrl', function ($scope) {
 
   	$scope.debugBtn = function() {
-    	console.log($scope.inputs);
+    	console.log($scope.inputs0);
+    	console.log($scope.inputs1);
     };
 
   	var i;
-    $scope.inputs = [];
+    $scope.inputs0 = [];
+    $scope.inputs1 = [];
     $scope.cntInputs = 1;
     
-    $scope.getTimes=function(){
+    $scope.addRow=function(){
 	    var a = [];
 
-	    for(i=1; i <= $scope.cntInputs; i++) {
+	    for(i=0; i <= $scope.cntInputs; i++) {
 			a.push(i);
 	    }
-
 	    return a;	    
 	};
 
 	$scope.addfield = function() {
     	$scope.cntInputs += 1;
     };
-
-    
-    $scope.getValue = function(){
-        //here get the value of that inserted in the element with the id of "input_" + id
-        return $scope.inputs[-1];
-    };
-
-
-
-
 
 
 });
