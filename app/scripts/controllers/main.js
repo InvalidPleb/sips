8e5b5142d37dcd29eaedcc79583184bc3a7c55a3
InvalidPleb <invalidpleb1@gmail.com>
2015-12-06 17:33:26 -0700

    Spreadsheet can now expand cols + rows

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,46 +14,38 @@ angular.module('statscalcApp')
   	$scope.debugBtn = function() {
     	console.log($scope.inputs0);
     	console.log($scope.inputs1);
+    	console.log($scope.cells);
     };
+
+	$scope.addRow = function() {
+    	$scope.rows.push($scope.rows.length + 1);
+
+    };
+
+    $scope.addCol = function(){
+    	$scope.columns.push('var' + ($scope.columns.length + 1));
+    };
+
+    $scope.columns = ['var1'];
+  	$scope.rows = [];
+  	$scope.cells = {};
+
 
   	var i;
-  	var j;
-    $scope.inputs0 = [];
-    $scope.inputs1 = [];
-    $scope.inputs2 = [];
-    $scope.cntInputs = 10;
-    
-    $scope.addRow=function(){
-	    var a = [];
+  	var numRows = 15;
+  	var numCols = 10;
 
-	    for(i=0; i <= $scope.cntInputs; i++) {
-			a.push(i);
-	    }
-	    return a;	    
-	};
+  	for (i=1; i <= numRows; i++) {
+  		$scope.rows.push(i);
+  	}
 
-	$scope.addCol=function(){
-	    var b = [];
-
-	    for(j=0; j <= $scope.cntInputs; j++) {
-			b.push(j);
-	    }
-	    return b;	    
-	};
-
-	$scope.addfield = function() {
-    	$scope.cntInputs += 1;
-    };
-
-
-    $scope.columns = ['A', 'B', 'C', 'D'];
-  	$scope.rows = [1, 2, 3, 4];
-  	$scope.cells = {};
+  	for (i=2; i <= numCols; i++) {
+  		$scope.columns.push('var' + i);
+  	}
   	
   	$scope.compute = function(cell) {
       return $parse($scope.cells[cell])($scope);
   	};
 
-    
 
 });
