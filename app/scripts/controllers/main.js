c5b3ca975b22140fe63d28d8ab503af63b4acad1
InvalidPleb <invalidpleb1@gmail.com>
2015-12-12 01:51:25 -0700

    Cols can be shortened & clear btn resets everything

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -26,10 +26,14 @@ angular.module('statscalcApp')
 
     $scope.addCol = function(){
     	$scope.columns.push('var' + ($scope.columns.length + 1));
+
+    	/*
     	
     	$('.tableBody').css({
     		"margin-left":"-=39px" 
     	});
+
+		*/
 		
     	countColumns += 1;
 
@@ -58,33 +62,59 @@ angular.module('statscalcApp')
   	}
   	
   	$scope.compute = function(cell) {
-      return $parse($scope.cells[cell])($scope);
+    	return $parse($scope.cells[cell])($scope);
   	};
 
   	$scope.clearBtn = function() {
 
-  		for (i=15; i < $scope.rows.length; i++) {
-	  		if (i >= 15) {
-	  			$scope.rows.splice(i);
+  		numRows = parseInt($scope.rows.length);
+  		numCols = parseInt($scope.columns.length);
+
+  		if ($scope.rows.length >= 15) {
+
+	  		for (i=15; i <= $scope.rows.length; i++) {
+		  		
+		  		$scope.rows.splice(i);
 	  		}
+
+  		} else {
+
+  			for (i = ($scope.rows.length + 1); i <= 15; i++) {
+
+  				$scope.rows.push(i);
+  			}
+
   		}
 
-  		for (i=10; i < $scope.columns.length; i++) {
-	  		if (i >= 10) {
-	  			$scope.columns.splice(i);
-	  		}
+  		if ($scope.columns.length >= 10) {
+
+	  		for (i=10; i < $scope.columns.length; i++) {
+		  		
+		  		$scope.columns.splice(i);
+	  		} 
+
+  		} else {
+
+  			for (i = ($scope.columns.length + 1); i <= 10; i++) {
+
+  				$scope.columns.push('var' + i);
+  			}
   		}
+
+  		$scope.inputRows = '';
+  		$scope.inputCols = '';
+  	};
+
+  	/* need to update for single additions w/ button push */
+
+  	$scope.updateInputRows = function() {
+
+  		numRows = parseInt($scope.rows.length);
   	};
 
   	$scope.updateInputCols = function() {
 
   		numCols = parseInt($scope.columns.length);
-  	};
-
-  	$scope.updateInputRows = function() {
-
-  		numRows = parseInt($scope.rows.length);
-
   	};
 
 
@@ -97,8 +127,8 @@ angular.module('statscalcApp')
   		if ($scope.inputRows !== '' && $scope.inputRows !== undefined && $scope.inputRows !== 0) {
 
   			if ($scope.inputRows > $scope.rows.length) {
-	  			pushRows = $scope.inputRows - $scope.rows.length;
 
+	  			pushRows = $scope.inputRows - $scope.rows.length;
 	  			for (i = ($scope.rows.length + 1); i <= (numRows + pushRows); i++) {
 	  				$scope.rows.push(i);
 	  			}
@@ -113,13 +143,15 @@ angular.module('statscalcApp')
   			}
   		}
 
-  		if ($scope.inputCols !== '' && $scope.inputCols !== undefined && $scope.inputRows !== 0) {
+  		if ($scope.inputCols !== '' && $scope.inputCols !== undefined && $scope.inputCols !== 0) {
   			if ($scope.inputCols > $scope.columns.length) {
-	  			pushCols = $scope.inputCols - $scope.columns.length;
 
-	  			for (i = ($scope.columns.length + 1); i <= (10 + pushCols); i++) {
+	  			pushCols = $scope.inputCols - $scope.columns.length;
+	  			for (i = ($scope.columns.length + 1); i <= (numCols + pushCols); i++) {
 	  				$scope.columns.push('var' + i);
 	  			}
+
+	  			/*
 
 	  			for (j = 1; j <= (i - 10); j++) {
 	  				
@@ -127,11 +159,16 @@ angular.module('statscalcApp')
     					"margin-left":"-=39px" 
     				});
 	  			}
-  			} 
-  		}
 
-  		numRows = 15;
-  		numCols = 10;
+	  			*/
+  			} else {
+
+  				pushCols = $scope.inputCols;
+  				for (i = ($scope.columns.length); i >= (pushCols); i--) {
+	  				$scope.columns.splice(i);
+	  			}
+  			}
+  		}
 
   	};
 
