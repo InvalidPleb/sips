f387679f6086d992127613e24c5c2cde4ec8e920
InvalidPleb <invalidpleb1@gmail.com>
2015-12-13 03:20:56 -0700

    Pearson's r calculations added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,6 +13,7 @@ angular.module('statscalcApp')
 
   	$scope.debugBtn = function() {
 
+  		console.log($scope.cells);
   		
     };
 
@@ -183,21 +184,61 @@ angular.module('statscalcApp')
 
   	var cellsSquaredX = [];
   	var cellsSquaredY = [];
-  	var cellsSum = [];
-  	var colX, colY;
+  	var cellsXY = [];
+  	var colXArr = [];
+  	var colYArr = [];
+  	var cellsCounter = 0;
+  	var colX, colY, colXSum, colYSum, cellsXYSum, cellsSquaredXSum, cellsSquaredYSum, numberSamples,
+  	rScore, rScore1, rScore2, rScore3, rScore4;
+
+  	function add(a, b) {
+    	return a + b;
+	}
 
   	$scope.calcTTest = function () {
 
-  		for (i=1; i <= 15; i++) {
+  		cellsCounter = 0;
 
-    		cellsSquaredX[i] = Math.pow($scope.cells['var1' + i], 2);
-    		cellsSquaredY[i] = Math.pow($scope.cells['var2' + i], 2);
-    		cellsSum[i] = parseInt($scope.cells['var1' + i]) + parseInt($scope.cells['var2' + i]);
+
+  		for (i=1; i <= $scope.rows.length; i++) {
+
+  			if (isNaN($scope.cells['var1' + i]) === false && isNaN($scope.cells['var2' + i]) === false) {
+
+  				colXArr[i] = parseInt($scope.cells['var1' + i]);
+  				colYArr[i] = parseInt($scope.cells['var2' + i]);
+  				cellsXY[i] = parseInt($scope.cells['var1' + i]) * parseInt($scope.cells['var2' + i]);
+	    		cellsSquaredX[i] = Math.pow($scope.cells['var1' + i], 2);
+	    		cellsSquaredY[i] = Math.pow($scope.cells['var2' + i], 2);
+
+	    		cellsCounter += 1;
+	    		
+
+    		} else {
+
+    			i = ($scope.rows.length + 1);
+    		}
     	}
 
-    	console.log(cellsSum);
+
+    	numberSamples = cellsCounter;
+    	colXSum = colXArr.reduce(add, 0);
+    	colYSum = colYArr.reduce(add, 0);
+    	cellsXYSum = cellsXY.reduce(add, 0);
+	    cellsSquaredXSum = cellsSquaredX.reduce(add, 0);
+	    cellsSquaredYSum = cellsSquaredY.reduce(add, 0);
+
+	    rScore1 = (numberSamples * cellsXYSum) - (colXSum * colYSum);
+	    rScore2 = (numberSamples * cellsSquaredXSum) - Math.pow(colXSum, 2);
+	    rScore3 = (numberSamples * cellsSquaredYSum) - Math.pow(colYSum, 2);
+	    rScore4 = Math.sqrt((rScore2 * rScore3));
+	    rScore = rScore1 / rScore4;
+
+	   	console.log(rScore);
+	    
 
   	};
+
+  	
 
 
 
